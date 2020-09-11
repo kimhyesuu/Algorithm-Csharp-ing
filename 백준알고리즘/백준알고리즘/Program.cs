dc571a51031ad0738403924c12b02c32decb5e30
kimhyesuu <you@example.com>
2020-09-11 13:11:27 +0900

    ConsoleAgorithm

---
+++ 백준알고리즘/백준알고리즘/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준알고리즘
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+
+         StringBuilder getNumber = new StringBuilder();
+         while (true)
+         {
+            getNumber.AppendLine(Console.ReadLine().ToString());
+            if (getNumber.ToString().Contains("0 0")) break;
+         }
+
+         StringReader readNumber = new StringReader(getNumber.ToString());
+         StringBuilder outNumber = new StringBuilder();
+         bool isFirst = true;
+
+         while (true)
+         {
+            string[] abGet = readNumber.ReadLine().Split();
+            int a = int.Parse(abGet[0]);
+            int b = int.Parse(abGet[1]);
+            int sum = a + b;
+            if (sum == 0) break;
+
+            if(isFirst)
+            {
+               isFirst = false;
+               outNumber.Append(sum.ToString());
+               continue;
+            }
+
+            outNumber.Append("\n").Append(sum.ToString());
+         }
+
+         Console.WriteLine(outNumber.ToString());
+         Console.ReadLine();
+      }
+   }
+}
---
+++ 백준알고리즘/사분면고르기/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 사분면고르기
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         int x = int.Parse(Console.ReadLine());
+         int y = int.Parse(Console.ReadLine());
+         int multiply = x * y;
+
+         if(multiply > 0)
+         {
+            if(x > 0 && y > 0)
+            {
+               Console.WriteLine(1);
+            }
+            else
+            {
+               Console.WriteLine(3);
+            }
+         }
+         else
+         {
+            if (x > 0 && y < 0)
+            {
+               Console.WriteLine(4);
+            }
+            else
+            {
+               Console.WriteLine(2);
+            }
+         }
+      }
+   }
+}
