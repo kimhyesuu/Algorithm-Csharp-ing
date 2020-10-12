de091e48ca1dd5c9c38701422e2708b931054092
kimhyesu <juid00@naver.com>
2020-10-12 14:11:40 +0900

    update

---
+++ 백준알고리즘/Consoleread/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoleread
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         int a = int.Parse(Console.ReadLine());
+         int r = int.Parse(Console.ReadLine());
+            int b = 1_000_000_000;
+         Console.WriteLine($"{r} {a}");
+         Console.ReadKey();
+      }
+   }
+}
